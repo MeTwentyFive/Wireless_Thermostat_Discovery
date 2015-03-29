175c2bf18979beb1bacc873eff68b6065cafeb3e
Christopher Winton <GitHubDevSpam@winton.cc>
2015-03-29 15:18:57 -0500

    Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Wireless_Thermostat_Discovery
-Simple class for discovering wireless thermostats.
+#Wireless Thermostat Discovery#
+
+Simple class for discovering Radio Thermostat brand wireless thermostats.
+
+##NOTE##
+
+If you are attempting to run this on Windows, if the SSDP service is running this class won't be able to detect anything.
--- src/com/twentyfive/Main.java
+++ src/com/twentyfive/Main.java
@@ -1,31 +1,32 @@
 package com.twentyfive;
 
 import com.sun.deploy.util.StringUtils;
-
 import java.util.List;
 
 
 //Simple test driver for the Wireless thermostat discovery
 //NOTE: If you are running on Windows and the SSDP service is running this will fail.
-public class Main {
+public class Main
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         ThermostatDiscovery discover = new ThermostatDiscovery();
-        String address = null;
 
         try
         {
             discover.Init();
             discover.Discover();
         }
-        catch (Exception e)
+        catch(Exception e)
         {
             System.out.println("Caught exception: " + e.toString());
         }
 
-        if (discover.FoundAddress()) {
-            List<String> thermostats = discover.GetAddresses();
-            String things = StringUtils.join(thermostats, ", ");
+        if( discover.FoundAddress() )
+        {
+            List<String>    thermostats = discover.GetAddresses();
+            String          things      = StringUtils.join(thermostats, ", ");
             System.out.println(things);
         }
 
